a0721ec9f6395437ccff77f7420d214f06d7c1c8
Роман Зорин <sgena170315@gmail.com>
2025-09-26 16:42:52 +0300

    Финальная версия программы. Была добавлена возможность вводить вес и рост с . и ,

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,8 +9,11 @@
 
         private void OnCalculateClicked(object sender, EventArgs e)
         {
-            if (!double.TryParse(WeightEntry.Text, out double weight) ||
-                !double.TryParse(HeightEntry.Text, out double heightCm))
+            string weightText = WeightEntry.Text?.Replace(',', '.');
+            string heightText = HeightEntry.Text?.Replace(',', '.');
+
+            if (!double.TryParse(weightText, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double weight) || //честно, часть кода не моя, нашел в интернете :)
+        !double.TryParse(heightText, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out double heightCm))
             {
                 ResultLabel.Text = "Введите правильные числа.";
                 ResultLabel.TextColor = Colors.Red;
@@ -66,7 +69,7 @@
         private Color GetCategoryColor(double bmi)
         {
             if (bmi < 18.5) return Color.FromArgb("#FFA726");
-            if (bmi < 25) return Color.FromArgb("#4CAF50");
+            if (bmi < 25) return Color.FromArgb("#39FF14");
             if (bmi < 30) return Color.FromArgb("#FF9800");
             return Color.FromArgb("#F44336");
         }
